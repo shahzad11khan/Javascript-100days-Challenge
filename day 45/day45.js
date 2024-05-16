bf2c7f45963d3528e7f3025eb89d70b7a3e2c5c5
shahzad11khan <61977658+shahzad11khan@users.noreply.github.com>
2024-05-16 08:25:20 +0500

    add new challenges

---
+++ day 45/day45.js
@@ -0,0 +1,18 @@
+// find the square of elements in the array array1 and check if that square exists in the array2
+let array1 = [1, 2, 3, 4, 7];
+let array2 = [4, 9, 12, 16, 76];
+
+for (let i = 0; i < array1.length; i++) {
+  for (let j = 0; j < array2.length; j++) {
+    let square = array1[i] * array1[i];
+    if (square === array2[j]) {
+      console.log(
+        "Square of",
+        array1[i],
+        "in array1 matches element",
+        array2[j],
+        "in array2."
+      );
+    }
+  }
+}
