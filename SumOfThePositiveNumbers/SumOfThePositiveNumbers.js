5a17e9e8a54f7c77156e4abdf48ed5637f3931d7
shahzad11khan <61977658+shahzad11khan@users.noreply.github.com>
2024-05-21 08:13:35 +0500

    new challenge add

---
+++ SumOfThePositiveNumbers/SumOfThePositiveNumbers.js
@@ -0,0 +1,12 @@
+// some the positive numbers in array
+let array = [1, 2, -9, -5, 9];
+
+let sum = 0;
+const positive = array.filter((num) => num > 0);
+console.log(`get the positive numbers from the array : ${positive}`);
+
+for (let el of positive) {
+  // console.log(el)
+  sum += el;
+}
+console.log(`Sum of the positive numbers is : ${sum}`);
---
+++ bubblesort/bubblesort.js
@@ -0,0 +1,15 @@
+// bubble sort algo
+
+let array = [1, 4, 6, 8, 9, 0, 10];
+const bubblesort = (array) => {
+  for (let index = array.length; index > 0; index--) {
+    for (let j = 0; j < index - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        [array[j], array[j + 1]] = [array[j + 1], array[j]];
+      }
+    }
+  }
+  return array;
+};
+const result = bubblesort(array);
+console.log(result);
