9e992a4c4faa5dc65a1238453f8b89f4e600b4c8
shahzad11khan <61977658+shahzad11khan@users.noreply.github.com>
2024-06-25 09:46:41 +0500

    JavaScript function that reverses strings in an array without using built-in functions

--- find the same prefix in the array/45.js
+++ find the same prefix in the array/45.js
@@ -13,3 +13,22 @@ const findprefex = (array) => {
 console.log(findprefex(["hi", "hu", "iy"])); //true
 console.log(findprefex(["hi", "iu", "py"])); // false
 console.log(findprefex(["hi", "hi", "ht"])); // true
+
+// combire first 2 character
+const findPrefix = (array) => {
+  if (array.length === 0) return false;
+
+  const firstPrefix = array[0].substring(0, 2);
+  console.log(firstPrefix);
+  for (let i = 1; i < array.length; i++) {
+    if (array[i].substring(0, 2) !== firstPrefix) {
+      return false;
+    }
+  }
+  return true; // All strings matched
+};
+
+console.log(findPrefix(["hi", "hu", "iy"])); // false
+console.log(findPrefix(["hi", "iu", "py"])); // false
+console.log(findPrefix(["hi", "hi", "ht"])); // true
+console.log(findPrefix(["hello", "helicopter", "hero"])); // true
---
+++ reverses strings in an array/reverses_strings_in_an_array.js
@@ -0,0 +1,22 @@
+// JavaScript function that reverses strings in an array without using built-in functions
+function reverseString(str) {
+  let reversedStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedStr += str[i];
+  }
+  return reversedStr;
+}
+
+function reverseStrings(inputArray) {
+  let outputArray = [];
+  for (let i = 0; i < inputArray.length; i++) {
+    console.log(inputArray[i]);
+    let reversedStr = reverseString(inputArray[i]);
+    outputArray.push(reversedStr);
+  }
+  return outputArray;
+}
+
+const input = ["hi", "hello", "shahzad", "Ahmad"];
+const output = reverseStrings(input);
+console.log(output); // ["ih", "olleh"]
